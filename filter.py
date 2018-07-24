9a3b0c99247b3c576f70d82713de5b593151bdf1
nkarcher <nkarcher@localhost>
2018-07-24 11:25:52 +0200

    Addig polymut.py; fixed small bug

--- filter.py
+++ filter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python3
 
 import pysam,sys
 import argparse
@@ -6,34 +6,59 @@ import numpy as np
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--minlen', help='Minimum breadth of alignment for a read to pass (i.e. 0.7 means 70% of the read-length must be aligning) ', type=float, default=0.70)
+parser.add_argument('--minlen', help='Minimum length of alignment for a read to pass', type=int, default=70)
 parser.add_argument('--minqual', help='Minimum average quality for a read to pass. It is computed over the fastq Phred-scores of each read', type=int, default=30)
-parser.add_argument('--maxsnps', help='Maximum SNPS+GAPS for a read to pass. It is computed over XM and XO values (Snps+gaps)', type=int, default=5)
-parser.add_argument('--logfile', help='output a logfile reporting the results')
+parser.add_argument('--maxsnps', help='Maximum edit distance on the alignment for a read to pass. It is computed over NM value from the BAM)', type=float, default=1.0)
+parser.add_argument('--exclude_targets', help='Exclude these entries (FASTA file to filter out)')
 
 args = parser.parse_args()
+if args.exclude_targets:
+	to_exclude = list(set([rec.strip() for rec in open(args.exclude_targets)]))
+else:
+	to_exclude = []
+
 samfile = pysam.AlignmentFile("-", "rb")
 passingReads = pysam.AlignmentFile("-", "wb", template=samfile)
 
-if args.logfile:
-	logFile = open(args.logfile,'w')
+#if args.logfile:
+#	logFile = open(args.logfile,'w')
 
 for read in samfile.fetch():
-	
-	if not read.is_secondary and float(read.query_alignment_length) / float(read.query_length) >= args.minlen and int(read.get_tag('NM')) <= args.maxsnps and np.mean(read.query_qualities[1]) >= args.minqual:
-		if args.logfile:
-			logFile.write(str(read.query_name)+"\tPASS\n")
+	#TODOTODO
+	alignment_len = int(read.query_alignment_length)
+	snps = read.get_tag('NM')
+	qualities = read.query_qualities
+	refname = read.reference_name
 
+	snps_rate =float(snps) / float(read.query_alignment_length)
+	meanqualities =np.mean(read.query_qualities)
+ 	
+#	print('--\nrefname: '+str(refname)+' alignment_len: '+str(alignment_len)+' snps: '+str(snps)+' snps_rate: '+str(snps_rate)+' qualities: '+str(qualities)+' meanqualities: '+str(meanqualities))
+
+	if (not read.is_secondary) and (alignment_len >= args.minlen) and (snps_rate <= args.maxsnps) and (meanqualities >= args.minqual) and (refname not in to_exclude):
+
+#	if not read.is_secondary and \
+#	   (int(read.query_alignment_length) >= args.minlen) and \
+#	   ((float(read.get_tag('NM')) / float(read.query_length)) <= args.maxsnps) and \
+#	   (np.mean(read.query_qualities) >= args.minqual) and \
+#	   (read.reference_name not in to_exclude):
+
+#		#print (read.query_name+'\t'+'PASS')
 		passingReads.write(read)
-	else:
-		if args.logfile:
-			if read.is_secondary: reason = "IS SECONDARY"
-			elif float(read.query_alignment_length) / float(read.query_length) < args.minlen: reason = "SHORT ALIGNMENT ("+str(float(read.query_alignment_length) / float(read.query_length)*100)+"% of the read-length)"
-			elif int(read.get_tag('NM')) > args.maxsnps: reason = "IS TOO SNIPPY ("+str( int(read.get_tag('NM')) )+")"
-			elif np.mean(read.query_qualities[1]) < args.minqual: reason = "IS TOO LOW QUALITY ("+str(np.mean(read.query_qualities[1]))+")"
-			logFile.write(str(read.query_name)+"\tREMOVE\t"+reason+'\n')
+#	else:
+		#print (read.query_name+'\t'+'EXCLUDE')
 
-passingReads.close()
-if args.logfile:
-	logFile.close()
+		
+
+##	else:
+##		if args.logfile: 
+##			if read.is_secondary: reason = "IS SECONDARY"
+##			elif (int(read.query_alignment_length) < args.minlen): reason = "SHORT ALIGNMENT ("+str(int(read.query_alignment_length))
+##			elif ((float(read.get_tag('NM')) / float(read.query_length)) > args.maxsnps): reason = "IS TOO SNIPPY ("+str( float(read.get_tag('NM')) / float(read.query_length) )+")"
+##			elif (np.mean(read.query_qualities) < args.minqual): reason = "IS TOO LOW QUALITY ("+str(np.mean(read.query_qualities))+")"
+##			elif (read.reference_name in to_exclude): reason = "IS IN THE EXCLUDE LIST"
+##
+##			logFile.write(str(read.query_name)+"\tREMOVE\t"+reason+'\n')
+#sys.exit(0)
+passingReads.close() 
 samfile.close()
--- polymut.py
+++ polymut.py
@@ -10,7 +10,6 @@ import sys
 
 def polymut_from_file(args):
 	import pandas as pd
-	import numpy as np
 
 	outputDicts=[]
 
@@ -22,10 +21,9 @@ def polymut_from_file(args):
 	if (args.gff_file):
 		bf.parse_gff(args.gff_file)
 
-	tl = bf.get_contigs_obj():
-	for i in tl:
+	for i in bf.get_contigs_obj():
 		dominanceArray, mutationStats = i.easy_polymorphism_rate(minqual=args.minqual,mincov=args.mincov,dominant_frq_thrsh=args.dominant_frq_thrsh)
-		outputDicts.append({'DN':mutationStats['DN'],'DS':mutationStats['DS'],'D?':mutationStats['D?'], "consid_pos":len([x for x in dominanceArray if x  != np.nan])})
+		outputDicts.append({'Ref':i.name, 'DN':mutationStats['DN'],'DS':mutationStats['DS'],'D?':mutationStats['D?'], "consid_pos":len([x for x in dominanceArray if not np.isnan(x)])})
 	out_df = pd.DataFrame.from_dict(outputDicts).set_index('Ref')
 	print float(np.sum(out_df["DN"])), float(np.sum(out_df["DS"])), float(sum(out_df["consid_pos"])) 
 
@@ -35,7 +33,9 @@ if __name__ == "__main__":
 	parser.add_argument('-c','--contig', help='Focus on a subset of references in the BAM file. Can be a list of references separated by commas or a FASTA file (the IDs are used to subset)', metavar="REFERENCE ID" ,default=None)
 	parser.add_argument('-f', help='If set unmapped (FUNMAP), secondary (FSECONDARY), qc-fail (FQCFAIL) and duplicate (FDUP) are excluded. If unset ALL reads are considered (bedtools genomecov style). Default: unset',action='store_true')
 	parser.add_argument('--sortindex', help='Sort and index the file',action='store_true')
+	parser.add_argument('--minlen', help='Minimum Reference Length for a reference to be considered. Default: '+str(CMSEQ_DEFAULTS.minlen),default=CMSEQ_DEFAULTS.minlen, type=int)
 	parser.add_argument('--minqual', help='Minimum base quality. Bases with quality score lower than this will be discarded. This is performed BEFORE --mincov. Default: 30', type=int, default=CMSEQ_DEFAULTS.minqual)
 	parser.add_argument('--mincov', help='Minimum position coverage to perform the polymorphism calculation. Position with a lower depth of coverage will be discarded (i.e. considered as zero-coverage positions). This is calculated AFTER --minqual. Default:'+str(CMSEQ_DEFAULTS.mincov), type=int, default=CMSEQ_DEFAULTS.mincov)
 	parser.add_argument('--dominant_frq_thrsh', help='Cutoff for degree of `allele dominance` for a position to be considered polymorphic. Default: '+str(CMSEQ_DEFAULTS.poly_dominant_frq_thrsh), type=float, default=CMSEQ_DEFAULTS.poly_dominant_frq_thrsh)
+	parser.add_argument('--gff_file', help="GFF file used to extract protein-coding genes", default = None)
 	polymut_from_file(parser.parse_args())
