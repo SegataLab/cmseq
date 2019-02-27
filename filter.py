f5e93ac513a3111aff7defb6a76e0d734fdeb7c4
Moreno <moreno.zolfo@gmail.com>
2019-02-27 03:29:44 +0100

    filter.py can now filter out reads basing on an external BAM file

--- filter.py
+++ filter.py
@@ -10,6 +10,7 @@ parser.add_argument('--minlen', help='Minimum length of alignment for a read to 
 parser.add_argument('--minqual', help='Minimum average quality for a read to pass. It is computed over the fastq Phred-scores of each read', type=int, default=30)
 parser.add_argument('--maxsnps', help='Maximum edit distance on the alignment for a read to pass. It is computed over NM value from the BAM)', type=float, default=1.0)
 parser.add_argument('--exclude_targets', help='Exclude these entries (FASTA file to filter out)')
+parser.add_argument('--exclude_reads_bam', help='Exclude these entries (BAM file to filter out)')
 
 args = parser.parse_args()
 if args.exclude_targets:
@@ -17,48 +18,29 @@ if args.exclude_targets:
 else:
 	to_exclude = []
 
+if args.exclude_reads_bam:
+	ex_samfile = pysam.AlignmentFile(args.exclude_reads_bam, "rb")
+	reads_to_exclude= list(set([''.join(i.query_name.split('_')[:-1]) for i in ex_samfile.fetch(until_eof=True)]))
+
+else:
+	reads_to_exclude = []
+
 samfile = pysam.AlignmentFile("-", "rb")
 passingReads = pysam.AlignmentFile("-", "wb", template=samfile)
 
-#if args.logfile:
-#	logFile = open(args.logfile,'w')
-
 for read in samfile.fetch():
-	#TODOTODO
 	alignment_len = int(read.query_alignment_length)
 	snps = read.get_tag('NM')
+
 	qualities = read.query_qualities
 	refname = read.reference_name
-
+	readname = read.query_name
 	snps_rate =float(snps) / float(read.query_alignment_length)
 	meanqualities =np.mean(read.query_qualities)
- 	
-#	print('--\nrefname: '+str(refname)+' alignment_len: '+str(alignment_len)+' snps: '+str(snps)+' snps_rate: '+str(snps_rate)+' qualities: '+str(qualities)+' meanqualities: '+str(meanqualities))
 
-	if (not read.is_secondary) and (alignment_len >= args.minlen) and (snps_rate <= args.maxsnps) and (meanqualities >= args.minqual) and (refname not in to_exclude):
-
-#	if not read.is_secondary and \
-#	   (int(read.query_alignment_length) >= args.minlen) and \
-#	   ((float(read.get_tag('NM')) / float(read.query_length)) <= args.maxsnps) and \
-#	   (np.mean(read.query_qualities) >= args.minqual) and \
-#	   (read.reference_name not in to_exclude):
-
-#		#print (read.query_name+'\t'+'PASS')
+	if (not read.is_secondary) and (alignment_len >= args.minlen) and (snps_rate <= args.maxsnps) and (meanqualities >= args.minqual) and (refname not in to_exclude) and (readname not in reads_to_exclude):
 		passingReads.write(read)
-#	else:
-		#print (read.query_name+'\t'+'EXCLUDE')
 
-		
 
-##	else:
-##		if args.logfile: 
-##			if read.is_secondary: reason = "IS SECONDARY"
-##			elif (int(read.query_alignment_length) < args.minlen): reason = "SHORT ALIGNMENT ("+str(int(read.query_alignment_length))
-##			elif ((float(read.get_tag('NM')) / float(read.query_length)) > args.maxsnps): reason = "IS TOO SNIPPY ("+str( float(read.get_tag('NM')) / float(read.query_length) )+")"
-##			elif (np.mean(read.query_qualities) < args.minqual): reason = "IS TOO LOW QUALITY ("+str(np.mean(read.query_qualities))+")"
-##			elif (read.reference_name in to_exclude): reason = "IS IN THE EXCLUDE LIST"
-##
-##			logFile.write(str(read.query_name)+"\tREMOVE\t"+reason+'\n')
-#sys.exit(0)
 passingReads.close() 
 samfile.close()
